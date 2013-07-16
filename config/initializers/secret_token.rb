89aff9b2688ffdc66b997784005bc05f36774f6b
Sidnei Pacheco <sidneip.junior@gmail.com>
2013-07-16 19:26:00 -0300

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kwik::Application.config.secret_token = 'f283133234a2a2b6f32ebb820f0659c3baa40dc55137fdb18818adfc4f1969acf69eff8a46873366b44865f763d1a3460df9dbcdc9bc23d9aaa6d7106197cd0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kwik::Application.config.session_store :cookie_store, key: '_kwik_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kwik::Application.config.session_store :active_record_store
