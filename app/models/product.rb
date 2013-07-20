0218e4e3043bfe8b0602f7ed74368e3ee6e45084
Sidnei Pacheco <sidneip.junior@gmail.com>
2013-07-20 14:02:26 -0300

    att access category_id

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  attr_accessible :descricao, :nome
+  attr_accessible :descricao, :nome, :category_id
   belongs_to :category
   validates_presence_of :nome, :descricao
 end
